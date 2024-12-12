551f4d6b9b23921cda62b84642a77764dce631bf
Nizar Soutafi <SOUTAFI_NIZAR@emsi-edu.ma>
2024-12-12 01:23:31 +0100

    last commit

--- src/main/java/ma/emsi/nizarsoutafi/tp1nizarsoutafi/jsf/Bb.java
+++ src/main/java/ma/emsi/nizarsoutafi/tp1nizarsoutafi/jsf/Bb.java
@@ -204,19 +204,16 @@ public class Bb implements Serializable {
         List<SelectItem> listeSystemRoles = new ArrayList<>();
         // Ces rôles ne seront utilisés que lorsque la réponse sera données par un LLM.
         String role = """
-                You are an interpreter. You translate from English to French and from French to English.
-                If the user type a French text, you translate it into English.
-                If the user type an English text, you translate it into French.
-                If the text contains only one to three words, give some examples of usage of these words in English.
+                You are a coding assistant. You can generate, debug, and explain code in various programming languages.\s
+                You can also provide code examples and tutorials.
                 """;
         //le 1er argument : la valeur du rôle, 2ème argument : le libellé du rôle
-        listeSystemRoles.add(new SelectItem(role, "Traducteur Anglais-Français"));
+        listeSystemRoles.add(new SelectItem(role, "Code Assistant"));
         role = """
-                Your are a travel guide. If the user type the name of a country or of a town,
-                you tell them what are the main places to visit in the country or the town
-                are you tell them the average price of a meal.
+                You are a technical writer. You can explain complex technical topics in a clear and concise manner.\s
+                You can also create tutorials and documentation.
                 """;
-        listeSystemRoles.add(new SelectItem(role, "Guide touristique"));
+        listeSystemRoles.add(new SelectItem(role, "Technical Writer"));
         return listeSystemRoles;
     }
 }
