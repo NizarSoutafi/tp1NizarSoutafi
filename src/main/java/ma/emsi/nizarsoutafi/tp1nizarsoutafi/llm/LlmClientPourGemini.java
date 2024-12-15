0bf4abf668829bc5ae5ad2a6de7a8bb4cdea5618
Nizar Soutafi <SOUTAFI_NIZAR@emsi-edu.ma>
2024-12-15 23:22:31 +0100

    Resoudre clé API commit

--- src/main/java/ma/emsi/nizarsoutafi/tp1nizarsoutafi/llm/LlmClientPourGemini.java
+++ src/main/java/ma/emsi/nizarsoutafi/tp1nizarsoutafi/llm/LlmClientPourGemini.java
@@ -12,11 +12,21 @@ import java.io.Serializable;
  */
 @Dependent
 public class LlmClientPourGemini implements Serializable {
-    private final String key = "AIzaSyC9O7NTcmXz7oSPpCTvhb-FdvumUT_w97Y";
-    private final Client clientRest = ClientBuilder.newClient();
+    // Clé pour l'API du LLM
+    private final String key;
+    // Client REST. Facilite les échanges avec une API REST.
+    private Client clientRest; // Pour pouvoir le fermer
+    // Représente un endpoint de serveur REST
     private final WebTarget target;
 
     public LlmClientPourGemini() {
+        // Récupère la clé secrète pour travailler avec l'API du LLM, mise dans une variable d'environnement
+        // du système d'exploitation.
+        this.key = System.getenv("GEMINI_KEY");
+        //A ECRIRE...
+        // Client REST pour envoyer des requêtes vers les endpoints de l'API d'OpenAI
+        this.clientRest = ClientBuilder.newClient();
+        // Endpoint REST pour envoyer la question à l'API.
         this.target = clientRest.target("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + this.key);
     }
 
